a33992dfc7886a59ee2d12a74fd1544cd33618b4
euis-rahma <108078975+euis-rahma@users.noreply.github.com>
2022-07-22 15:16:52 +0700

    Add files via upload

---
+++ js/file.js
@@ -0,0 +1,58 @@
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+$(document).ready(function() {
+$('.table-paginate').dataTable();
+} );
+
+    function myFunction() {
+      document.getElementById("myDropdown").classList.toggle("show-side");
+    }
+    
+    function filterFunction() {
+      var input, filter, ul, li, a, i;
+      input = document.getElementById("myInput");
+      filter = input.value.toUpperCase();
+      div = document.getElementById("myDropdown");
+      a = div.getElementsByTagName("a");
+      for (i = 0; i < a.length; i++) {
+        txtValue = a[i].textContent || a[i].innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          a[i].style.display = "";
+        } else {
+          a[i].style.display = "none";
+        }
+      }
+    }
+
+   
+
+    if($(window).width() < 767)
+{
+    document.getElementById("sidebarToggleTop").click()
+}
